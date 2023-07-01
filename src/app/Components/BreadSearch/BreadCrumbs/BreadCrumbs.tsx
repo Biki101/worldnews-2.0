c7498ce99e199c542dea01bef67a028e71aa4163
Biki <biki@core>
2023-07-01 16:16:34 +0545

    added breadcrumbs

--- src/app/Components/BreadSearch/BreadCrumbs/BreadCrumbs.tsx
+++ src/app/Components/BreadSearch/BreadCrumbs/BreadCrumbs.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { ImHome } from "react-icons/im";
+import { FaRegNewspaper } from "react-icons/fa";
 const BreadCrumbs = () => {
-  return <div>BreadCrumbs</div>;
+  return (
+    <div className="flex items-center gap-3 text-[18px] opacity-60">
+      <div className="flex items-center gap-2">
+        <ImHome />
+        <h1>WORLD NEWS</h1>
+      </div>
+      /
+      <div className="flex items-center gap-2">
+        <FaRegNewspaper />
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
 };
 
 export default BreadCrumbs;
