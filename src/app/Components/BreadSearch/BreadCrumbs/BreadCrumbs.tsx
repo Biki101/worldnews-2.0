dbf72c801b4eab049869e3284d6b2a7215f667fb
udiprai <udiprai.co@gmail.com>
2023-07-25 13:53:22 +0545

    added dynamic content to burger

--- src/app/Components/BreadSearch/BreadCrumbs/BreadCrumbs.tsx
+++ src/app/Components/BreadSearch/BreadCrumbs/BreadCrumbs.tsx
@@ -1,7 +1,16 @@
+"use client";
 import React from "react";
 import { ImHome } from "react-icons/im";
 import { FaRegNewspaper } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import { BiWorld } from "react-icons/bi";
+import { BsFillStarFill } from "react-icons/bs";
+import { MdConnectWithoutContact, MdSportsSoccer } from "react-icons/md";
+import { FiHelpCircle } from "react-icons/fi";
+
 const BreadCrumbs = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <div className="flex items-center gap-3 text-[18px] opacity-60">
       <div className="flex items-center gap-2">
@@ -10,8 +19,39 @@ const BreadCrumbs = () => {
       </div>
       /
       <div className="flex items-center gap-2">
-        <FaRegNewspaper />
-        <h1>Home</h1>
+        {pathname === "/" ? (
+          <>
+            <FaRegNewspaper />
+            <h1>Home</h1>
+          </>
+        ) : pathname === "/global" ? (
+          <>
+            <BiWorld />
+            <h1>World</h1>
+          </>
+        ) : pathname === "/populars" ? (
+          <>
+            <BsFillStarFill />
+            <h1>Top Stories</h1>
+          </>
+        ) : pathname === "/sports" ? (
+          <>
+            <MdSportsSoccer />
+            <h1>Sports</h1>
+          </>
+        ) : pathname === "/about" ? (
+          <>
+            <FiHelpCircle />
+            <h1>About</h1>
+          </>
+        ) : pathname === "/contact" ? (
+          <>
+            <MdConnectWithoutContact />
+            <h1>Contact</h1>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
