2164d0c03b21d0921103cf79245744914b24d99b
udiprai <udiprai.co@gmail.com>
2023-07-30 09:47:21 +0545

    adjusted search bar

--- src/app/Components/BreadSearch/SearchComponent/SearchComponent.tsx
+++ src/app/Components/BreadSearch/SearchComponent/SearchComponent.tsx
@@ -7,10 +7,10 @@ const SearchComponent = () => {
       <input
         type="text"
         placeholder="What can I help you with today?"
-        className="outline-none border-[1px] border-black p-3 w-[270px]"
+        className="outline-none border-[1px] border-black text-[12px] md:text-[16px] p-2 pl-4 lg:p-3 w-[270px]"
       />
-      <div className="text-white bg-black p-3 border-[1px] border-black">
-        <MdOutlineTravelExplore className="text-[26px]" />
+      <div className="text-white bg-black p-2 lg:p-3 border-[1px] border-black">
+        <MdOutlineTravelExplore className="text-[20px] md:text-[26px]" />
       </div>
     </div>
   );
