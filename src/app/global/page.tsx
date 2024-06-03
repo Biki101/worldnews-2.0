eec6bb2f50d9913323418a7a777f0a0ff34e54f7
Biki101 <thakurbiki52@gmail.com>
2024-06-03 15:49:40 +0545

    fixed page name

--- src/app/global/page.tsx
+++ src/app/global/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import PopularSection from "../Components/MainNews/PopularSection/PopularSection";
 import GlobalMain from "./global-main/GlobalMain";
 import axios from "axios";
 
-const page = () => {
+const Global = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -34,4 +34,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Global;
